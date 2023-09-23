e0568211b90ab3a535567d17eaf122a55ffd60f7
Carlos Peñaloza <carlos2172336@correo.uis.edu.co>
2023-09-23 13:53:53 -0500

    23/09/2023: Test EndPoint added

---
+++ src/main/java/com/cdc/tuhome/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.cdc.tuhome.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/test")
+public class TestController {
+
+    @GetMapping
+    ResponseEntity<String> test(){
+        return  ResponseEntity.ok("Test OK");
+    }
+
+
+}
